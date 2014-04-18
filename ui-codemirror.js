4678e7118e747936578d0c76586da6ac4b9facbc
Lauri Peltonen <lpeltonen@nvidia.com>
2014-04-18 18:16:28 +0300

    Updates

--- ui-codemirror.js
+++ ui-codemirror.js
@@ -1,4 +1,7 @@
 'use strict';
+/**
+ * Binds a CodeMirror widget to a <textarea> element.
+ */
 angular.module('ui.codemirror', []).constant('uiCodemirrorConfig', {}).directive('uiCodemirror', [
   'uiCodemirrorConfig',
   function (uiCodemirrorConfig) {
@@ -7,9 +10,14 @@ angular.module('ui.codemirror', []).constant('uiCodemirrorConfig', {}).directive
       require: '?ngModel',
       priority: 1,
       compile: function compile(tElement) {
+        // Require CodeMirror
         if (angular.isUndefined(window.CodeMirror)) {
           throw new Error('ui-codemirror need CodeMirror to work... (o rly?)');
         }
+        // Create a codemirror instance with
+        // - the function that will to place the editor into the document.
+        // - the initial content of the editor.
+        //   see http://codemirror.net/doc/manual.html#api_constructor
         var value = tElement.text();
         var codeMirror = new window.CodeMirror(function (cm_el) {
             angular.forEach(tElement.prop('attributes'), function (a) {
@@ -19,6 +27,7 @@ angular.module('ui.codemirror', []).constant('uiCodemirrorConfig', {}).directive
                 cm_el.setAttribute(a.name, a.textContent);
               }
             });
+            // FIX replaceWith throw not parent Error !
             if (tElement.parent().length <= 0) {
               tElement.wrap('<div>');
             }
@@ -39,17 +48,18 @@ angular.module('ui.codemirror', []).constant('uiCodemirrorConfig', {}).directive
           if (angular.isDefined(scope.$eval(iAttrs.uiCodemirror))) {
             scope.$watch(iAttrs.uiCodemirror, updateOptions, true);
           }
+          // Specialize change event
           codeMirror.on('change', function (instance) {
             var newValue = instance.getValue();
             if (ngModel && newValue !== ngModel.$viewValue) {
               ngModel.$setViewValue(newValue);
-            }
-            if (!scope.$$phase) {
               scope.$apply();
             }
           });
           if (ngModel) {
-            ngModel.$formatters.push(function (value) {
+            // CodeMirror expects a string, so make sure it gets one.
+            // This does not change the model.
+            ngModel.$formatters.unshift(function (value) {
               if (angular.isUndefined(value) || value === null) {
                 return '';
               } else if (angular.isObject(value) || angular.isArray(value)) {
@@ -57,18 +67,25 @@ angular.module('ui.codemirror', []).constant('uiCodemirrorConfig', {}).directive
               }
               return value;
             });
+            // Override the ngModelController $render method, which is what gets called when the model is updated.
+            // This takes care of the synchronizing the codeMirror element with the underlying model, in the case that it is changed by something else.
             ngModel.$render = function () {
+              //Code mirror expects a string so make sure it gets one
+              //Although the formatter have already done this, it can be possible that another formatter returns undefined (for example the required directive)
               var safeViewValue = ngModel.$viewValue || '';
               codeMirror.setValue(safeViewValue);
             };
           }
+          // Watch ui-refresh and refresh the directive
           if (iAttrs.uiRefresh) {
             scope.$watch(iAttrs.uiRefresh, function (newVal, oldVal) {
+              // Skip the initial watch firing
               if (newVal !== oldVal) {
                 codeMirror.refresh();
               }
             });
           }
+          // onLoad callback
           if (angular.isFunction(opts.onLoad)) {
             opts.onLoad(codeMirror);
           }
